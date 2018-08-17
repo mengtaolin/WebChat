04b968c945fdb82e8a10aa4f15fffcad6e6f69b2
1187352588@qq.com <1187352588@qq.com>
2018-08-17 14:38:08 +0800

    居然有问题
    还他妈找不到。回去好好找找

---
+++ public/js/chat.js
@@ -0,0 +1,39 @@
+var Chat = function(socket){
+    this.socket = socket;
+}
+
+Chat.prototype.sendMessage = function(room, text){
+    var message = {
+        room: room,
+        text:text
+    }
+    this.socket.emit('message', message);
+}
+
+Chat.prototype.changeRoom = function(room){
+    this.socket.emit('join', {
+        newRoom: room
+    });
+};
+
+Chat.prototype.processCommand = function(command){
+    var words = command.split(" ");
+    var subCommand = words[0].subString(1, words[0].length).toLowerCase();
+    var message = false;
+    switch(subCommand){
+        case 'join':
+            words.shift();
+            var room = words.join(' ');
+            this.changeRoom(room);
+            break;
+        case 'nick':
+            words.shift();
+            var name = words.join(' ');
+            this.socket.emit('nameAttempt', name);
+            break;
+        default:
+            message = 'Unrecognized command.';
+            break;
+    }
+    return message;
+}
